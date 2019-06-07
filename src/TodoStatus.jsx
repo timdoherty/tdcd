7e266eeb7ed56b3d975d2ef18362da1019d7f2ef
Tim Doherty <tim.doherty@procore.com>
2019-06-07 10:42:59 -0700

    add proptypes to todo status component
    (cherry picked from commit 13e859ff7aba81989171fed445e0f2595cafab9a)

--- src/TodoStatus.jsx
+++ src/TodoStatus.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function TodoStatus(props) {
   return (
@@ -9,5 +10,10 @@ function TodoStatus(props) {
     />
   );
 }
+
+TodoStatus.propTypes = {
+  isComplete: PropTypes.bool,
+  onChange: PropTypes.func.isRequired,
+};
 
 export default TodoStatus;
