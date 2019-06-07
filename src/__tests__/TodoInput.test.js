670c3f530a4a6d0de3f3369c98310bb14cff6084
Tim Doherty <tim.doherty@procore.com>
2019-06-07 11:46:31 -0700

    switch input from useRef to useState
    (cherry picked from commit ce02ffb0bc1b00c8b5693ef2bd41012959c973cc)

--- src/__tests__/TodoInput.test.js
+++ src/__tests__/TodoInput.test.js
@@ -7,14 +7,16 @@ describe('<TodoInput/>', () => {
   describe('given a place to enter a new task', () => {
     describe('when a user presses enter', () => {
       describe('and the task name is not empty', () => {
-        it('responds with the new task name', () => {
+        it('responds with the new task name and clears the new task name', () => {
           const todo = 'you do it too';
           const onKeyUpMock = jest.fn();
-          const wrapper = mount(<TodoInput onKeyUp={onKeyUpMock} />);
+          const wrapper = shallow(<TodoInput onKeyUp={onKeyUpMock} />);
 
-          wrapper.simulate('keyup', { key: 'Enter', target: { value: todo } });
+          wrapper.simulate('change', { target: { value: todo } });
+          wrapper.simulate('keyup', { key: 'Enter' });
 
           expect(onKeyUpMock).toHaveBeenCalledWith(todo);
+          expect(wrapper.prop('value')).toBe('');
         });
       });
 
