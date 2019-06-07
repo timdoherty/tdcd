f5b332553d266280e85507f029241b99ff31b087
Tim Doherty <tim.doherty@procore.com>
2019-06-06 21:43:54 -0700

    clear input after enter

--- src/TodoInput.jsx
+++ src/TodoInput.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 function TodoInput(props) {
+  const inputRef = useRef();
   return (
     <input
+      ref={inputRef}
       onKeyUp={e => {
         if (e.key === 'Enter' && e.target.value) {
           props.onKeyUp(e.target.value);
+          inputRef.current.value = '';
         }
       }}
     />
